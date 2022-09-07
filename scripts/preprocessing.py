333b48a2350c2f7208715d95eb5b34e855e5b10e
Yohans Samuel <yohanssamuel2014@gmail.com>
2022-09-07 11:32:54 +0300

    add save_csv function

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,16 +1,41 @@
 import pandas as pd
 from logger import Logger
 import sys
+from sklearn.preprocessing import Normalizer, MinMaxScaler, StandardScaler
+import pickle
+import dvc.api
+
+
+app_logger = Logger("preprocessing.log").get_app_logger()
 
 class Preprocessing:
     def __init__(self) -> None:
         try:
-            self.logger = Logger("logger.log").get_app_longer()
-            self.logger.info("class is successfully instantiated")
+            pass
         except Exception:
-            self.logger.info("class is not instantiated")
             sys.exit(1)
 
-    def read_csv(self,path) -> pd.DataFrame:
+    def read_csv(self,csv_path,missing_values=[]) -> pd.DataFrame:
         # open and read csv files given the path to the file
-        return pd.read_csv(path)
+        try:
+            df = pd.read_csv(csv_path, na_values=missing_values)
+            print("file read as csv")
+            self.logger.info(f"file read as csv from {csv_path}")
+            return df
+        except FileNotFoundError:
+            print("file not found")
+            self.logger.error(f"file not found, path:{csv_path}")
+
+    def save_csv(self, df, csv_path):
+        try:
+            df.to_csv(csv_path, index=False)
+            print('File Successfully Saved.!!!')
+            self.logger.info(f"File Successfully Saved to {csv_path}")
+
+        except Exception:
+            print("Save failed...")
+            self.logger.error(f"saving failed")
+
+        return df
+
+    
