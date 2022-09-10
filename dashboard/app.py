751af420052835a4255d2cbb3a79bc7ee46ebab1
Yohans Samuel <yohanssamuel2014@gmail.com>
2022-09-10 21:35:44 +0300

    update dashboard

--- dashboard/app.py
+++ dashboard/app.py
@@ -6,7 +6,7 @@ import streamlit as st
 import plotly.graph_objects as go
 # 
 import sys
-sys.path.insert(0,'./scripts/')
+# sys.path.insert(0,'./scripts/')
 
 #
 import seaborn as sns
@@ -14,15 +14,27 @@ import matplotlib.pyplot as plt
 
 
 # displays user overview analysis
-def customer_behavior_analysis():
-    st.title("Customer Behavior Analysis")
+def customer_purchase_behavior():
+    st.title("Customer Purchase Behavior Analysis")
 # 
 def home():
-    st.title("Rossmann Pharmaceutical Sales forecasting")
+    st.markdown("<h1 style='padding:2rem;text-align:left; \
+            background-color:blue;color:black;font-size:1.8rem;\
+            border-radius:0.2rem;'>Rosmann Pharmaceuticals Sales Prediction</h1>", 
+            unsafe_allow_html=True)
+    st.write("---")
+    st.write(
+    """
+   The finance team at Rossmann Pharmaceuticals wants to forecast 
+   sales in all their stores across several cities six weeks ahead of time.
+    """
+    )
 
-    st.markdown(
-        "The finance team at Rossmann Pharmaceuticals wants to forecast sales in all their stores across several cities six weeks ahead of time.")
-# 
+    st.subheader("The dataset")
+    st.write(" the dataset should be of the following form:")
+    st.write(pd.read_csv("./data/cleaned_train.csv"))
+
+    st.subheader("Sales trend")
 def sales_prediction_ml():
     st.title("Sales Prediction using Machine Learning(Random Forest)")
     st.header('Sample Data from the experience dataset')
@@ -34,7 +46,7 @@ def sales_prediction_dl():
 
 pages = {
     "Welcome Page":home,
-    "Customer Behavior Analysis": customer_behavior_analysis,
+    "Customer Behavior Analysis": customer_purchase_behavior,
     "Sales Prediction using Random Forest": sales_prediction_ml,
     "Sales Prediction using LSTM": sales_prediction_dl,
 }
