f92e57f2ac38f1c746ad722ae914e3c72fcdceef
Yohans Samuel <yohanssamuel2014@gmail.com>
2022-09-10 22:45:31 +0300

    update the dashboard

--- dashboard/app.py
+++ dashboard/app.py
@@ -12,11 +12,12 @@ import sys
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+import datetime
 
 # displays user overview analysis
 def customer_purchase_behavior():
-    st.title("Customer Purchase Behavior Analysis")
-# 
+    st.title("Customer Purchase Behavior")
+
 def home():
     st.markdown("<h1 style='padding:2rem;text-align:left; \
             background-color:blue;color:black;font-size:1.8rem;\
@@ -35,17 +36,64 @@ def home():
     st.write(pd.read_csv("./data/cleaned_train.csv"))
 
     st.subheader("Sales trend")
+
 def sales_prediction_ml():
     st.title("Sales Prediction using Machine Learning(Random Forest)")
-    st.header('Sample Data from the experience dataset')
+    number = st.number_input('Enter a StoreID')
+    st.write('The enterd StoreID is ', number)
+
+    d = st.date_input("Choose a Date of Prediction", datetime.date(2019, 7, 6))
+    st.write('The selected date is:', d)
+
+    prediction_day = st.radio(
+     "Choose a prediction date",
+     ('Monday', 'Tuesday', 'Wednesday','Thursday','Friday'))
+
+    if prediction_day == 'Monday':
+        st.write('You selected Monday.')
+    elif prediction_day == 'Tuesday':
+        st.write('You selected Tuesday.')
+    elif prediction_day == 'Wednesday':
+        st.write('You selected Wednesday.')
+    elif prediction_day == 'Thursday':
+        st.write('You selected Thursday.')
+    else:
+        st.write("You selected Friday.")
+
+    promotion_day = st.radio(
+     "Promotion day ?",('Yes', 'No'))
+
+    if promotion_day == 'Yes':
+        st.write('You selected Yes.')
+    else:
+        st.write("You selected No.")
+
+    school_holiday = st.radio(
+     "School Holiday",('Yes', 'No'))
+
+    if school_holiday == 'Yes':
+        st.write('You selected Yes.')
+    else:
+        st.write("You selected No.")
+
+    state_holiday = st.radio(
+     "State Holiday",('None', 'Christmas','Easter Holiday','Public Holiday'))
+
+    if state_holiday == 'None':
+        st.write('You selected None.')
+    elif state_holiday == 'Christmas':
+        st.write('You selected Christmas.')
+    elif state_holiday == 'Easter Holiday':
+        st.write('You selected Easter Holiday.')
+    else:
+        st.write("You selected Public Holiday.")
 
 def sales_prediction_dl():
     st.title("Sales Prediction using Deep Learning(LSTM)")
-    st.header('Sample Data from the experience dataset')
     
 
 pages = {
-    "Welcome Page":home,
+    "Home":home,
     "Customer Behavior Analysis": customer_purchase_behavior,
     "Sales Prediction using Random Forest": sales_prediction_ml,
     "Sales Prediction using LSTM": sales_prediction_dl,
